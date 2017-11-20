a8467015dc3e416e5c159bfa6c84026ffd0d6e29
Michael Carroll <juwara0@users.noreply.github.com>
2017-11-20 14:28:49 -0600

    fix publishing

--- .travis/maybe-publish-coverage.sh
+++ .travis/maybe-publish-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_NODE_VERSION" != "stable" ]
+if [ "$TRAVIS_NODE_VERSION" != "6.9.1" ]
 then
   echo "Skipping coverage publish for TRAVIS_NODE_VERSION ${TRAVIS_NODE_VERSION}"
   exit 0
