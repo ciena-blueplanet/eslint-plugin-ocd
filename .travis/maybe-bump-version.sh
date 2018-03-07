c066f3c893223961da115f4eb222ea5177086429
Michael Carroll <juwara0@users.noreply.github.com>
2018-03-07 08:02:05 -0600

    update to use pr-bumber version 3

--- .travis/maybe-bump-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_NODE_VERSION" != "6.11.0" ]
-then
-  echo "Skipping version bump for TRAVIS_NODE_VERSION ${TRAVIS_NODE_VERSION}"
-  exit 0
-fi
-
-$(npm root -g)/pr-bumper/.travis/maybe-bump-version.sh
